4a94f1f01118c0d6888322a73cd7758e168fd726
peterZhang66 <17600196554@163.com>
2018-01-03 15:56:58 +0800

    项目2：官网小程序展示

---
+++ pages/index/index.js
@@ -0,0 +1,13 @@
+Page({
+  data: {
+    imgUrls: [
+      '../../img/banner1.jpg',
+      '../../img/banner2.jpg',
+      '../../img/banner3.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
+  }
+})
